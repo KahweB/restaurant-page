9681d38d72e30b551ca0ed4e56e45b03459e3066
Kahve <72060765+KahweB@users.noreply.github.com>
2023-07-12 01:38:35 +0300

    home page updated

--- src/homepage.js
+++ src/homepage.js
@@ -15,6 +15,25 @@ export default function createHome() {
     logo.innerHTML = "Dante Redd";
     const description = document.createElement("div");
     description.setAttribute("id","description");
+    description.innerHTML = "It's finger-lickin' good";
+
+    //bottom section container
+    const bottomContainer = document.createElement("div");
+    bottomContainer.setAttribute("id","bottom-container");
+
+    //bottom section inside container
+    const orderSection = document.createElement("div");
+    orderSection.setAttribute("id","order-container");
+    //order now
+    const orderNow = document.createElement("div");
+    orderNow.setAttribute("id", "order-now");
+    orderNow.innerHTML = "Order Now"
+    //order logo one
+    const orderLogoOne = document.createElement("div");
+    orderLogoOne.setAttribute("id","order-logo-one");
+    //order logo two
+    const orderLogoTwo = document.createElement("div");
+    orderLogoTwo.setAttribute("id","order-logo-two");
 
 
     //appends
@@ -22,5 +41,9 @@ export default function createHome() {
     container.appendChild(about);
     about.appendChild(logo);
     about.appendChild(description);
-
+    content.appendChild(bottomContainer);
+    bottomContainer.appendChild(orderSection);
+    orderSection.appendChild(orderNow);
+    orderSection.appendChild(orderLogoOne);
+    orderSection.appendChild(orderLogoTwo);
 }
